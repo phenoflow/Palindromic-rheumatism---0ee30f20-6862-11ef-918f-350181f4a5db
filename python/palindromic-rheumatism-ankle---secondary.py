67a6316c3a3d96f56585ef94f6fdc80ea4ef2865
Martin Chapman <contact@martinchapman.co.uk>
2024-09-01 14:02:12 +0100

    Update made by Phenoflow

---
+++ python/palindromic-rheumatism-ankle---secondary.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"1570269","system":"ICD10CM"},{"code":"45538672","system":"ICD10CM"},{"code":"45548308","system":"ICD10CM"},{"code":"45601336","system":"ICD10CM"},{"code":"1570269","system":"ICD10CM"},{"code":"45601336","system":"ICD10CM"},{"code":"45538672","system":"ICD10CM"},{"code":"45548308","system":"ICD10CM"},{"code":"44832651","system":"ICD10CM"},{"code":"44832651","system":"ICD10CM"},{"code":"1570269","system":"ICD10CM"},{"code":"45538672","system":"ICD10CM"},{"code":"45548308","system":"ICD10CM"},{"code":"45601336","system":"ICD10CM"},{"code":"1570269","system":"ICD10CM"},{"code":"45601336","system":"ICD10CM"},{"code":"45538672","system":"ICD10CM"},{"code":"45548308","system":"ICD10CM"},{"code":"44832651","system":"ICD10CM"},{"code":"44832651","system":"ICD10CM"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('palindromic-rheumatism-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["palindromic-rheumatism-ankle---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["palindromic-rheumatism-ankle---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["palindromic-rheumatism-ankle---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
