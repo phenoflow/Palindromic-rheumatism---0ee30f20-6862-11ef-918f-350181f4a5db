63f0080b7b1dacc59d8827b5f0c6d07e9e124d59
Martin Chapman <contact@martinchapman.co.uk>
2024-09-01 14:03:01 +0100

    Update made by Phenoflow

---
+++ python/right-palindromic-rheumatism---secondary.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"45562634","system":"ICD10CM"},{"code":"45567453","system":"ICD10CM"},{"code":"45582041","system":"ICD10CM"},{"code":"45562634","system":"ICD10CM"},{"code":"45582041","system":"ICD10CM"},{"code":"45567453","system":"ICD10CM"},{"code":"45562634","system":"ICD10CM"},{"code":"45567453","system":"ICD10CM"},{"code":"45582041","system":"ICD10CM"},{"code":"45562634","system":"ICD10CM"},{"code":"45582041","system":"ICD10CM"},{"code":"45567453","system":"ICD10CM"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('palindromic-rheumatism-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["right-palindromic-rheumatism---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["right-palindromic-rheumatism---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["right-palindromic-rheumatism---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
